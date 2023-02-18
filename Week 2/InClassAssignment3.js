a7752f6a0466ca5ef08f823fb651e3f8647b0895
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-02-18 03:37:44 -0500

    week 2 end

---
+++ Week 2/InClassAssignment3.js
@@ -0,0 +1,27 @@
+let num = 0
+function FizzBuzz() {
+    
+    num += 1;
+    if (num > 100) return;
+
+    // You will see how much easier this is with React in the future...
+    let fbTag = document.getElementById("fizzbuzz");
+    if (num%3==0 && num%5==0) {
+        fbTag.innerHTML += '<span class="fizzbuzz">FizzBuzz</span><br>';
+    }
+    else if (num%3==0) {
+        fbTag.innerHTML += '<span class="fizz">Fizz</span><br>';
+    }
+    else if (num%5==0) {
+        fbTag.innerHTML += '<span class="buzz">Buzz</span><br>';
+    }
+    else {
+        fbTag.innerHTML += num + '<br>';
+    }
+}
+
+function RunAllFB() {
+    for (let i = 0; i <= 100; i++) {
+        FizzBuzz();
+    }
+}
