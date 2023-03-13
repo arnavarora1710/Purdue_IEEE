3b1f7a03db8176a5b58ac142a0361aa2dbf93907
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-03-12 22:43:00 -0400

    demo app.js setup

--- Week 3/lesson/src/App.js
+++ Week 3/lesson/src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Demo2 from './components/Demo2';
+import Demo4 from './components/Demo4';
 
 function App() {
+  const [showDefault, setShowDefault] = useState()
+  const [showDemo2, setShowDemo2] = useState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="default">
+      <button onClick={() => { setShowDefault(!showDefault) }}>Show Default App</button>
+        {showDefault ? <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div> : null }
+      </div>
+      <div id="Demo2">
+        <button onClick={ () => { setShowDemo2(!showDemo2) } }>Show Demo 2</button>
+        {showDemo2 ? <Demo2 /> : null }
+      </div>
+      <Demo4 />
+    </>
   );
 }
 
