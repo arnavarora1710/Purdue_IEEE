928a969040c14c3418ae93b3671d76e88a5d8bd5
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-03-25 16:03:22 -0400

    week 4 done

---
+++ Week 4/checkpoint/src/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../style/header.css';
+import ImagePath from '../img/pikachu-field.jpg';
+
+export function Header(props) {
+      // Return some JSX here...
+      return (
+        <div id="header">
+          <div id="container">
+            <div id="name">
+              <h1>
+                {props.name}
+              </h1>
+            </div>
+            <div id="hp">
+              <h1>
+                {props.hp}
+              </h1>
+            </div>
+          </div>
+          {<img src = {ImagePath} alt = "Cute Pikachu in the field :)" />}
+        </div>
+      );
+}
---
+++ Week 4/checkpoint/src/components/info.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import '../style/info.css';
+
+export function Info(props) {
+
+    // TODO: create a timestamp useState() variable with initial value set
+    // to the timestamp attribute from props
+    const [timeStamp, setTimeStamp] = useState(props.timestamp);
+    
+    
+    const updateTimestamp = () => {
+      // TODO: update the timestamp with the current date and time. 
+      // Use (new Date()).toISOString() to get the current date and time.
+      setTimeStamp((new Date()).toISOString())
+    }
+
+    // Text defaults. Feel free to add your own!
+    const move = "Quick Attack"
+    const description = "Flip a coin. If heads, this attack does 10 more damage."
+    const damage = "10+"
+
+
+    // Return some JSX here...
+    return (
+      <div id="info">
+        <div id="move-header">
+          <div id="move">
+            <h3>{move}</h3>
+          </div>
+          <div id="damage">
+            <h3>{damage}</h3>
+          </div>
+        </div>
+        <div id="description">
+          <p>{description} </p>
+        </div>
+        <div id="timestamp">
+          Loaded at {timeStamp}
+        </div>
+
+        <div id="button-container">
+          {<button onClick = {updateTimestamp}>Update Timsetamp</button>}
+        </div>
+      </div>
+    );
+}
